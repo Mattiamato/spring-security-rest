4743da1553bd254962f3b12256a4d5c02411e589
Mattia Amato <mattiamato@gmail.com>
2017-09-26 18:59:53 +0200

    Enable HTTPS and redirect HTTP to HTTPS

--- src/main/java/ch/mamato/spring/Application.java
+++ src/main/java/ch/mamato/spring/Application.java
@@ -2,9 +2,15 @@ package ch.mamato.spring;
 
 import java.util.Arrays;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -46,6 +52,9 @@ public class Application {
 		return new InMemoryTokenStore();
 	}
 
+	/*
+	 * Init user
+	 */
 	@Autowired
 	public void authenticationManager(AuthenticationManagerBuilder builder, UserRepository userRepo) throws Exception {
 		if (userRepo.count() == 0) {
@@ -54,5 +63,39 @@ public class Application {
 		}
 		builder.userDetailsService(username -> new CustomUserDetails(userRepo.findByUsername(username)))
 				.passwordEncoder(passwordEncoder());
+	}
+
+	/*
+	 * Redirect HTTP to HTTPS
+	 */
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+		return tomcat;
+	}
+
+	/*
+	 * Redirect HTTP to HTTPS
+	 */
+	private Connector initiateHttpConnector() {
+		Connector connector = new Connector(TomcatEmbeddedServletContainerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+
+		return connector;
 	}
 }
